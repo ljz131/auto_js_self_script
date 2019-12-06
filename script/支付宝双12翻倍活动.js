0bf4da9aebae510a3b397c0c64ac4c3b6c789906
lizhefu <lizhefu@konka.com>
2019-12-06 11:43:04 +0800

    feat:初次提交脚本

---
+++ script/支付宝双12翻倍活动.js
@@ -0,0 +1,48 @@
+
+auto.waitFor();
+
+var height = device.height;
+var width = device.width;
+toast("\n设备宽" + width + "\n" + "设备高" + height + "\n" + "手机型号" + device.model + "\n安卓版本" + device.release)
+
+setScreenMetrics(width, height);
+autoplay();
+
+function autoplay(){
+    doAction("去完成");
+    
+    toast("结束");
+}
+
+function doAction(act){
+    while(textContains(act).exists()){        
+        toast("存在" + act);
+        textContains(act).findOne().click();
+        while(textContains("拒绝").exists()){
+            textContains("拒绝").findOne().click();
+            toast("拒绝");
+            sleep(1600);
+        }
+        while(textContains("不允许").exists()){
+            textContains("不允许").findOne().click();
+            toast("不允许");
+            sleep(1600);
+        }
+        var tmpTime = 2000 + Math.random()*5000;
+        toast(tmpTime);
+        sleep(tmpTime);
+        back();
+        while(textContains("再玩玩").exists()){
+            textContains("再玩玩").findOne().click();
+            toast("再玩玩");
+            sleep(1600);
+        }
+        while(textContains("系统繁忙").exists()){
+            toast("系统繁忙");
+            sleep(1600);
+        }
+        sleep(1600);
+    }
+    toast("完成[" + act + "]检测");
+    sleep(2000);
+}
