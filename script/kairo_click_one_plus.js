5445bf7a109eb3f6266f471b0a98ce1aeeec1f3d
lizhefu <lizhefu@konka.com>
2020-02-26 15:05:10 +0800

    新增开罗连续点击脚本

---
+++ script/kairo_click_one_plus.js
@@ -0,0 +1,12 @@
+setScreenMetrics(1080,2280);
+for(var i=0;i<=100;i++){
+    click(530,2020);
+    sleep(500);
+    click(530,2020);
+    sleep(500);
+    press(530,2020,2000);
+    click(530,2020);
+    sleep(500);
+    click(530,2020);
+    sleep(500);
+}
